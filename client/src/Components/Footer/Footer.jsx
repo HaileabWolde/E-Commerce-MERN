af016360d1deb1d0283b066e5ef003af7ed66a66
Haileab <womahaileab1428@gmail.com>
2023-12-12 10:24:36 +0300

    started working on e-commerce

---
+++ client/src/Components/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Row, Col, Container } from "react-bootstrap";
+
+const Footer = ()=>{
+return (
+    <Container>
+        <Row>
+            <Col className="text-center">
+            <span>Copyright &copy; E-commerce App</span>
+            </Col>
+        </Row>
+    </Container>
+)
+}
+export default Footer
