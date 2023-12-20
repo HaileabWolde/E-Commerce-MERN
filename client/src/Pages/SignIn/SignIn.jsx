f37c64fe7bdbb43ff7cb5d21cf88171a351282ba
Haileab <womahaileab1428@gmail.com>
2023-12-20 09:55:15 +0300

    sign in changes

--- client/src/Pages/SignIn/SignIn.jsx
+++ client/src/Pages/SignIn/SignIn.jsx
@@ -1,12 +1,17 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { useSelector } from 'react-redux'
 import {Card, Container, Form, Button} from 'react-bootstrap'
 import {useDispatch} from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 import { Login } from '../../actions/userAction'
+import Message from '../../Components/shared/Message/message'
+import Loading from '../../Components/shared/Spinner/CircularLoading'
 const SignIn = ()=>{
     const dispatch = useDispatch()
     const navigate= useNavigate()
     const [check, setCheck] = useState(false)
+    const [Error, setError] = useState(false)
+    const { error, isloading} = useSelector((state)=> state.user)
     const [data, setData] = useState({
         name: "",
         email: "",
@@ -21,7 +26,7 @@ const SignIn = ()=>{
     }
     const handleSubmit = (e) => {
       e.preventDefault();
-    
+
       if (data.name || data.confirmpassword) {
         dispatch(Login(data, navigate));
       } else {
@@ -32,6 +37,24 @@ const SignIn = ()=>{
     const handleSignup = ()=>{
       setCheck((prev)=> !prev)
     }
+    
+    useEffect(()=>{
+      if(error){
+        setError(true)
+      }
+    }, [error])
+
+    useEffect(() => {
+      if (Error) {
+        const timeoutId = setTimeout(() => {
+          setError(false);
+        }, 2000);
+    
+        return () => {
+          clearTimeout(timeoutId);
+        };
+      }
+    }, [Error]);
     return (
         <Container fluid>
              <Card className=' md:max-w-lg md:mx-auto mt-10 p-5 rounded-lg shadow-md'>
@@ -55,8 +78,10 @@ const SignIn = ()=>{
 
       <Form.Group className="mb-3" controlId="formBasicPassword">
         <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" value={data.password} name="password" onChange={handleChange}/>
+        <Form.Control type="password" className="relative"placeholder="Password" value={data.password} name="password" onChange={handleChange}/>
+        <i class="fa-regular fa-eye" className='absolute top-2'></i>
       </Form.Group>
+     
       {
          check &&  
           <Form.Group className="mb-3" controlId="confirmPassword">
@@ -64,10 +89,15 @@ const SignIn = ()=>{
             <Form.Control type="password" placeholder="Enter Password" value={data.confirmpassword} name= "confirmpassword" onChange={handleChange}/>
             </Form.Group>
               }
-      <Button variant="primary" type="submit" className='w-full'>
+      <Button variant="primary" type="submit" className='w-full mb-3'>
         {
-          check ? 'SIGN UP': 'SIGN IN'
+          isloading ?  <Loading/> :  <>
+          {
+            check ? 'SIGN UP': 'SIGN IN'
+          } 
+          </>
         }
+       
       </Button>
       <button onClick={handleSignup} type="button" className='text-center w-full'> {check ? <p>
         <span>have an account</span>
@@ -77,6 +107,12 @@ const SignIn = ()=>{
         <span >Doesn't have an account</span>
         <span className='text-blue-500 ml-2'>Please Sign Up</span>
         </p>}</button>
+        {
+          Error && 
+          <Message variant='danger'>
+            {error}
+          </Message>
+        }
     </Form>
             </Card>
         </Container>
